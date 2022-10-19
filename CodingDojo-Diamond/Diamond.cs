6507687b8f2f4e69a3ebd57fe08ec2cbe31d984c
Jérémy Hure <hure.j@sfeir.com>
2022-10-19 16:32:46 +0200

    Diamond works

--- CodingDojo-Diamond/Diamond.cs
+++ CodingDojo-Diamond/Diamond.cs
@@ -10,7 +10,71 @@ namespace CodingDojo_Diamond
     {
         public static string Create(char c)
         {
-            return "";
+            if (c < 'A' || c > 'Z') { return ""; }
+            if (c == 'A') { return "A"; }
+            string ret = "";
+            for (int spaces = 0; spaces < c - 'A'; spaces++)
+            {
+                ret += ' ';
+            }
+            ret += 'A';
+            for (int spaces = 0; spaces < c - 'A'; spaces++)
+            {
+                ret += ' ';
+            }
+            ret += '\n';
+            for (int i='B'; i<c; i++)
+            {
+                for (int spaces = 0; spaces < c - i; spaces++)
+                {
+                    ret += ' ';
+                }
+                ret += (char)i;
+                for (int spaces = 0; spaces <= 2 * (i-'B'); spaces++)
+                {
+                    ret += ' ';
+                }
+                ret += (char)i;
+                for (int spaces = 0; spaces < c - i; spaces++)
+                {
+                    ret += ' ';
+                }
+                ret += '\n';
+            }
+            ret += c;
+            for (int spaces = 0; spaces <= 2 * (c-'B'); spaces++)
+            {
+                ret += ' ';
+            }
+            ret += c + "\n";
+            for (int i = c - 1; i > 'A'; i--)
+            {
+                for (int spaces = 0; spaces < c - i; spaces++)
+                {
+                    ret += ' ';
+                }
+                ret += (char)i;
+                for (int spaces = 0; spaces <= 2 * (i - 'B'); spaces++)
+                {
+                    ret += ' ';
+                }
+                ret += (char)i;
+                for (int spaces = 0; spaces < c - i; spaces++)
+                {
+                    ret += ' ';
+                }
+                ret += '\n';
+            }
+            for (int spaces = 0; spaces < c - 'A'; spaces++)
+            {
+                ret += ' ';
+            }
+            ret += 'A';
+            for (int spaces = 0; spaces < c - 'A'; spaces++)
+            {
+                ret += ' ';
+            }
+            return ret;
         }
     }
 }
